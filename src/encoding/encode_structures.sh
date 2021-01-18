edddab7218746722b64ac0398de6559e4fe6c12b
Unknown <dominique.sydow@posteo.de>
2021-01-18 15:51:30 +0100

    Update kissim encoding bash script

--- src/encoding/encode_structures.sh
+++ src/encoding/encode_structures.sh
@@ -1,2 +1,7 @@
-kissim-encoder -i "../../data/processed/structure_klifs_ids.txt" -o "../../results/fingerprints.json" -c 6
-#kissim-encoder -i 109 118 110 113 111 116 112 114 115 117 -o "../../results/fingerprints.json" -c 7 -l "../../data/external/20201223_KLIFS_ABL2_HUMAN"
+#!/bin/bash
+
+# Generate kissim fingerprints for a set of KLIFS structures
+
+kissim encode -i "../../data/processed/structure_klifs_ids.txt" -o "../../results/fingerprints.json" -c 7 -l "../../data/external/20210114_KLIFS_HUMAN"
+#kissim encode -i "../../data/processed/structure_klifs_ids_test.txt" -o "../../results/fingerprints.json" -c 7 -l "../../data/external/20210114_KLIFS_HUMAN"
+#kissim encode -i 109 118 110 113 111 116 112 114 115 117 -o "../../results/fingerprints.json" -c 7 -l "../../data/external/20201223_KLIFS_ABL2_HUMAN"
