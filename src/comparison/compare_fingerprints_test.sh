96145def858d8f97453b8bb59e29f1f0d9a20380
dominiquesydow <dominique.sydow@posteo.de>
2021-03-19 11:32:13 +0100

    Add bash scripts

---
+++ src/comparison/compare_fingerprints_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Compare fingerprints
+
+kissim compare -i "../../results/fingerprints_test.json" -o "../../results/" -c 7
---
+++ src/encoding/encode_structures_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Generate kissim fingerprints for a set of KLIFS structures
+
+kissim encode -i "../../data/processed/structure_klifs_ids_test.txt" -o "../../results/fingerprints_test.json" -c 7 -l "../../data/external/20210114_KLIFS_HUMAN"
