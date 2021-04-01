6c99e2321f13c2aae8c5f5cfec60fcc0b576c19c
Unknown <dominique.sydow@posteo.de>
2021-04-01 18:52:30 +0200

    Set #cores to 16

--- src/pipeline/curta.sh
+++ src/pipeline/curta.sh
@@ -3,7 +3,7 @@
 #SBATCH --mail-user=dominique.sydow@fu-berlin.de
 #SBATCH --mail-type=begin,end
 #SBATCH --nodes=1
-#SBATCH --ntasks=32
+#SBATCH --ntasks=16
 #SBATCH --mem-per-cpu=4096
 #SBATCH --time=10:00:00
 #SBATCH --qos=standard
@@ -41,7 +41,7 @@
 
 KISSIM_APP=/home/${USER}/kissim_app
 RESULTS=$KISSIM_APP/results
-NCORES=32
+NCORES=16
 
 # Encode structures
 kissim encode -i $KISSIM_APP/data/processed/structure_klifs_ids.txt -o $RESULTS/fingerprints.json -c $NCORES -l $KISSIM_APP/data/external/20210114_KLIFS_HUMAN
