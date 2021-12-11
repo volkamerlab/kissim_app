e656c9548a9b5e77d95a6e150c07f64a3a2b3e62
dominiquesydow <dominique.sydow@posteo.de>
2021-12-11 16:47:37 +0100

    Satisfy black # flake8

--- src/data/interacting_residues.py
+++ src/data/interacting_residues.py
@@ -6,9 +6,11 @@ import numpy as np
 import pandas as pd
 from opencadd.databases.klifs import setup_remote
 
+
 def residues_interacting_with_ligand(ligand_expo_id):
     """
-    For a query ligand, get all interacting residues across all available ligand-bound structures in KLIFS.
+    For a query ligand, get all interacting residues across all available ligand-bound
+    structures in KLIFS.
     Based on KLIFS IFP (covers interactions between ligands and KLIFS pocket).
 
     Parameters
@@ -23,12 +25,14 @@ def residues_interacting_with_ligand(ligand_expo_id):
     """
 
     session = setup_remote()
-    
+
     # Get ligand KLIFS ID
     ligand_klifs_id = session.ligands.by_ligand_expo_id(ligand_expo_id)["ligand.klifs_id"][0]
-    
+
     # Get all IFPs for this ligand
-    ifp_series = session.interactions.by_ligand_klifs_id(ligand_klifs_id)["interaction.fingerprint"]
+    ifp_series = session.interactions.by_ligand_klifs_id(ligand_klifs_id)[
+        "interaction.fingerprint"
+    ]
     print(f"Number of IFPs for {ligand_expo_id}: {len(ifp_series)}")
     ifp_series_bool = ifp_series.apply(lambda x: [True if i == "1" else False for i in x])
     # Prepare multi-index for columns
@@ -37,7 +41,7 @@ def residues_interacting_with_ligand(ligand_expo_id):
     # Generate DataFrame
     ifp_df = pd.DataFrame(np.array(ifp_series_bool.to_list()), columns=columns_index)
 
-    # Number of features per residue 
+    # Number of features per residue
     n_features_per_residue = ifp_df.groupby("residue", axis=1).sum().sum()
     # Print stats
     print("(residue position, number of features across all IFPs)")
